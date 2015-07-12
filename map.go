85e0cafc24851bb3ed498528592056b492c56515
Keita Sone <k-sone@users.noreply.github.com>
2015-07-12 10:55:03 +0900

    fix sort order when a key is a null-terminated string

--- map.go
+++ map.go
@@ -1,24 +1,28 @@
 package critbitgo
 
+import (
+	"unsafe"
+)
+
 // The map is sorted according to the natural ordering of its keys
 type SortedMap struct {
 	trie *Trie
 }
 
 func (m *SortedMap) Contains(key string) bool {
-	return m.trie.Contains(mapStrToKey(key))
+	return m.trie.Contains(*(*[]byte)(unsafe.Pointer(&key)))
 }
 
 func (m *SortedMap) Get(key string) (value interface{}) {
-	return m.trie.Get(mapStrToKey(key))
+	return m.trie.Get(*(*[]byte)(unsafe.Pointer(&key)))
 }
 
-func (m *SortedMap) Set(key string, value interface{}) {
-	m.trie.Set(mapStrToKey(key), value)
+func (m *SortedMap) Set(key string, value interface{}) error {
+	return m.trie.Set([]byte(key), value)
 }
 
 func (m *SortedMap) Delete(key string) bool {
-	return m.trie.Delete(mapStrToKey(key))
+	return m.trie.Delete(*(*[]byte)(unsafe.Pointer(&key)))
 }
 
 func (m *SortedMap) Clear() {
@@ -33,7 +37,7 @@ func (m *SortedMap) Size() int {
 func (m *SortedMap) Keys() []string {
 	keys := make([]string, 0, m.Size())
 	m.trie.Allprefixed([]byte{}, func(k []byte, v interface{}) bool {
-		keys = append(keys, mapKeyToStr(k))
+		keys = append(keys, string(k))
 		return true
 	})
 	return keys
@@ -43,30 +47,11 @@ func (m *SortedMap) Keys() []string {
 // if handle returns `false`, the iteration is aborted.
 func (m *SortedMap) Each(prefix string, handle func(key string, value interface{}) bool) bool {
 	return m.trie.Allprefixed([]byte(prefix), func(k []byte, v interface{}) bool {
-		return handle(mapKeyToStr(k), v)
+		return handle(string(k), v)
 	})
 }
 
 // Create a SortedMap
 func NewSortedMap() *SortedMap {
 	return &SortedMap{NewTrie()}
-}
-
-func mapStrToKey(s string) []byte {
-	// avoid a KeyTailNull
-	if l := len(s); l > 0 {
-		if t := s[l-1]; t == 0x00 || t == 0xff {
-			return append([]byte(s), 0xff)
-		}
-	}
-	return []byte(s)
-}
-
-func mapKeyToStr(k []byte) string {
-	if l := len(k); l > 0 {
-		if t := k[l-1]; t == 0xff {
-			return string(k[:l-1])
-		}
-	}
-	return string(k)
 }
